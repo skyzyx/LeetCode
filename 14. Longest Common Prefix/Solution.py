19cbb3183a80a532455df5a7dc152fd384ecd14b
Jie Zhang <jiez@ucsc.edu>
2015-04-11 13:57:55 -0700

    add Solution.py

---
+++ 14. Longest Common Prefix/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @return a string
+    def longestCommonPrefix(self, strs):
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+            
+        for i in range(0, len(strs[0])):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[0][i] != strs[j][i]:
+                    return strs[0][0:i]
+        
+        return strs[0]
