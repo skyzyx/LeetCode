df1385d3cfa6c3e4c27613b817b87258c34d8fc8
Jie Zhang <jiez@ucsc.edu>
2015-04-10 23:21:08 -0700

    first commit

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (!map.containsKey(numbers[i])) {
+                map.put(target - numbers[i], i+1);
+            } else {
+                res[0] = map.get(numbers[i]);
+                res[1] = i+1;
+                return res;
+            }
+        }
+        return res;
+    }
+}
+
