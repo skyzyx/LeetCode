1ba5b2a33fde4ab08f698416e9c85e72e82a88f2
Jie Zhang <jiez@ucsc.edu>
2015-04-17 13:34:43 -0700

    Find the majority element.

---
+++ 169. Majority Element/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int majorityElement(int[] num) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < num.length; i++) {
+            if (!map.containsKey(num[i]))
+                map.put(num[i], 0); // Why not map.put(num[i], 1)
+            else
+                map.put(num[i], map.get(num[i]) + 1);
+                
+            if (map.get(num[i]) >= num.length / 2)
+                return num[i];
+        }
+        return num[0];
+    }
+}
+
+/*
+public class Solution {
+    public int majorityElement(int[] num) {
+        int majIndex = 0;
+        int majCount = 1;
+        
+        for (int i = 1; i < num.length; i++) {
+            if (num[majIndex] == num[i]) {
+                majCount++;
+            } else {
+                majCount--;
+            }
+            
+            if (majCount == 0) {
+                majIndex = i;
+                majCount = 1;
+            }
+        }
+        
+        return num[majIndex];
+    }
+}
+*/
