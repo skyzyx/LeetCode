ba3bb12d5bf40fee9be0153360f24f81dbeb5d7b
Jie Zhang <jiez@ucsc.edu>
2015-04-21 17:37:11 -0700

    Generate parentheses.

---
+++ Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        if (n <= 0)
+            return res;
+            
+        res.add("()");
+        
+        for (int i = 1; i < n; i++) {
+            ArrayList<String> tmp = new ArrayList<String>();
+            for (String word: res) {
+                for (int j = 0; j < word.length(); j++) {
+                    String s = word.substring(0, j) + "()" + word.substring(j);
+                    if (!tmp.contains(s))
+                        tmp.add(s);
+                }
+            }
+            res = new ArrayList<String>(tmp);
+        }
+        return res;
+    }
+}
