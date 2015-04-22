21b7ed253b15a67723b2737b8dc23d0d8d3ac8fe
Jie Zhang <jiez@ucsc.edu>
2015-04-21 21:27:16 -0700

    Implement pow(x, n).

---
+++ 50. Pow(x, n)/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param x, a float
+    # @param n, a integer
+    # @return a float
+    def pow(self, x, n):
+        if n == 0:
+            return 1.0
+        if n == 1:
+            return x
+        
+        sign = n
+        if n < 0:
+            n = -n
+            
+        res = self.pow(x, n/2)
+        if n % 2 == 0:
+            res = res * res
+        else:
+            res = res * res * x
+            
+        return res if sign > 0 else 1/res
+        
