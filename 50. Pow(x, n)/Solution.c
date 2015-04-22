fa6a776540768f89be350bfdf7155e6896d77240
Jie Zhang <jiez@ucsc.edu>
2015-04-21 21:25:14 -0700

    Implement pow(x, n).

---
+++ 50. Pow(x, n)/Solution.c
@@ -0,0 +1,18 @@
+double myPow(double x, int n) {
+    if (n == 0)
+        return 1.0;
+    if (n == 1)
+        return x;
+        
+    int sign = n;
+    if (n < 0)
+        n = -n;
+        
+    double res = myPow(x, n/2);
+    if (n % 2 == 0)
+        res = res * res;
+    else
+        res = res * res * x;
+        
+    return sign > 0? res: 1/res;
+}
