688e3b6918af020bcf5f63c4d3bf15d5f5294ffa
Jie Zhang <jiez@ucsc.edu>
2015-04-21 22:29:13 -0700

    Implement int sqrt(int x).

---
+++ 69. Sqrt(x)/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int mySqrt(int x) {
+        if (x < 0)
+            return -1;
+        if (x == 0)
+            return 0;
+            
+        long left = 0;
+        long right = x / 2 + 1; // sqrt(x) < x/2 + 1
+        
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long square = mid * mid;
+            if (square == x)
+                return (int)mid;
+            else if (square > x)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return (int)right;
+    }
+}
