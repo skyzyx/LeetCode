a0697b5ba8906411ed68d3d0c34adf9f99e5ab2a
Jie Zhang <jiez@ucsc.edu>
2015-04-22 21:01:00 -0700

    House Robber.

---
+++ 198. House Robber/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def rob(self, nums):
+        if len(nums) == 0:
+            return 0;
+            
+        dp = [0] * (len(nums) + 1)
+        dp[1] = nums[0]
+        
+        for i in range(2, len(nums) + 1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+            
+        return dp[len(nums)]
+        
