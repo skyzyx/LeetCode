fe73e2136dff3d73cd713ad36e1aa648b96f327b
Jie Zhang <jiez@ucsc.edu>
2015-04-22 22:43:58 -0700

    Reverse digits of an integer.

---
+++ 7. Reverse Integer/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int digit = 0;
+        long res = 0;
+        while (x != 0) {
+            digit = x % 10;
+            res = res * 10 + digit;
+            x /= 10;
+        }
+        
+        if (res > INT_MAX || res < INT_MIN)
+            return 0;
+        else
+            return (int)res;
+    }
+};
