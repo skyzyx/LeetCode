cc485acf1a9faa70d886aad7357f90ef3fa5b51d
Jie Zhang <jiez@ucsc.edu>
2015-04-22 22:42:25 -0700

    Reverse digits of an integer.

---
+++ 7. Reverse Integer/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param {integer} x
+    # @return {integer}
+    def reverse(self, x):
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+        res = 0
+        
+        while x != 0:
+            res = res * 10 + x % 10
+            x /= 10
+           
+        res = res * sign
+        if res > math.pow(2, 31) - 1 or res < math.pow(2, 31) * (-1):
+            return 0
+        else:
+            return res
