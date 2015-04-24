81caece0447e95111ba012f7e5015138c38fd6b7
Jie Zhang <jiez@ucsc.edu>
2015-04-24 11:01:19 -0700

    Given an index k, return the kth row of the Pascal's triangle.

---
+++ 119. Pascal's Triangle II/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (rowIndex < 0)
+            return res;
+            
+        res.add(1);
+        for(int i = 0; i < rowIndex; i++) {
+            for(int j = res.size() - 1; j > 0; j--) {
+                res.set(j, res.get(j) + res.get(j-1));
+            }
+            res.add(1);
+        }
+        return res;
+    }
+}
