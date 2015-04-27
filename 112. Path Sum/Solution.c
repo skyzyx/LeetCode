0bd2ea55172bf8269431811588b9375722642888
Jie Zhang <jiez@ucsc.edu>
2015-04-26 23:01:56 -0700

    Determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.

---
+++ 112. Path Sum/Solution.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+  * };
+ */
+bool hasPathSum(struct TreeNode *root, int sum) {
+    if (root == NULL)
+        return false;
+        
+    if (root->left == NULL && root->right == NULL && root->val == sum)
+        return true;
+        
+    return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+}
