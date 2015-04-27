343fe1b7165d764df057afcae5726c9b55b893b2
Jie Zhang <jiez@ucsc.edu>
2015-04-27 15:53:42 -0700

    Add two numbers which are representated by linked list.

---
+++ 2. Add Two Numbers/Solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+            
+        ListNode* res = new ListNode(-1); 
+        ListNode* dummyHead = res;
+        
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL) {
+            if (l1 != NULL) {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+            
+            if (l2 != NULL) {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            
+            res->next = new ListNode(carry % 10);
+            res = res->next;
+            carry /= 10;
+        }
+        
+        if (carry == 1)
+            res->next = new ListNode(carry);
+            
+        return dummyHead->next;
+    }
+};
