4517bd8fdfb877995c2efbb55581b8577b92d830
Jie Zhang <jiez@ucsc.edu>
2015-04-28 21:16:32 -0700

    Add one dimensional dynamic programming solution.

--- 63. Unique Paths II/Solution.java
+++ 63. Unique Paths II/Solution.java
@@ -30,3 +30,26 @@ public class Solution {
         return dp[m-1][n-1];
     }
 }
+
+// Solution 2.
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0)
+            return 0;
+        
+        int[] res = new int[obstacleGrid[0].length];
+        res[0] = 1;
+        
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            for (int j = 0; j < obstacleGrid[0].length; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    res[j] = 0;
+                } else {
+                    if (j > 0)
+                        res[j] += res[j-1];
+                }
+            }
+        }
+        return res[obstacleGrid[0].length - 1];
+    }
+}
