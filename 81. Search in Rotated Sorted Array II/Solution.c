13fa4130024325b2dc8335edeb1a37cd66adb24b
Jie Zhang <jiez@ucsc.edu>
2015-05-06 23:33:39 -0700

    Search in rotated sorted array (contains duplicates).

---
+++ 81. Search in Rotated Sorted Array II/Solution.c
@@ -0,0 +1,27 @@
+bool search(int* nums, int numsSize, int target) {
+    if (numsSize == 0)
+            return false;
+            
+    int l = 0;
+    int r = numsSize - 1;
+    while (l <= r) {
+        int m = (l + r) / 2;
+        if (nums[m] == target)
+            return true;
+                
+        if (nums[l] < nums[m]) {
+            if (target >= nums[l] && target <= nums[m])
+                r = m-1;
+            else
+                l = m+1;
+        } else if (nums[l] > nums[m]) {
+            if (target >= nums[m] && target <= nums[r])
+                l = m+1;
+            else
+                r = m-1;
+        } else {
+            l++;
+        }
+    }
+    return false;
+}
