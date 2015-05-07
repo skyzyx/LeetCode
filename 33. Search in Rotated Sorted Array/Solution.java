eb60d35d0d2750a53aaa43df8dcb25928e133a48
Jie Zhang <jiez@ucsc.edu>
2015-05-06 21:10:09 -0700

    Search in rotated sorted array (without duplicates).

---
+++ 33. Search in Rotated Sorted Array/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+            
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) { // Not l < r.
+            int m = (l + r) / 2;
+            if (nums[m] == target)
+                return m;
+            
+            if (nums[l] < nums[m]) {
+                if (target >= nums[l] && target < nums[m])
+                    r = m-1;
+                else
+                    l = m+1;
+            } else if (nums[l] > nums[m]){
+                if (target > nums[m] && target <= nums[r])
+                    l = m+1;
+                else
+                    r = m-1;
+            } else {
+                l++;
+            }
+        }
+        return -1;
+    }
+}
