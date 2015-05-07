4593862508034fbb1f51e68e720ffb2f3ed6a088
Jie Zhang <jiez@ucsc.edu>
2015-05-06 23:32:53 -0700

    Search in rotated sorted array (contains duplicates).

---
+++ 81. Search in Rotated Sorted Array II/Solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        if (nums.size() == 0)
+            return false;
+            
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target)
+                return true;
+                
+            if (nums[l] < nums[m]) {
+                if (target >= nums[l] && target <= nums[m])
+                    r = m-1;
+                else
+                    l = m+1;
+            } else if (nums[l] > nums[m]) {
+                if (target >= nums[m] && target <= nums[r])
+                    l = m+1;
+                else
+                    r = m-1;
+            } else {
+                l++;
+            }
+        }
+        return false;
+    }
+};
