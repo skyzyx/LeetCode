83c43526f8412c2a5ba8e23d6d64832d63ef9300
Jie Zhang <jiez@ucsc.edu>
2015-05-08 11:52:52 -0700

    Evaluate reverse polish notation.

---
+++ 150. Evaluate Reverse Polish Notation/Solution.py
@@ -0,0 +1,35 @@
+class Solution:
+    # @param {string[]} tokens
+    # @return {integer}
+    def evalRPN(self, tokens):
+        if tokens == None or len(tokens) == 0:
+            return 0
+            
+        stack = []
+        for word in tokens:
+            if word not in ["+", "-", "*", "/"]:
+                stack.append(int(word))
+            else:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                
+                if word == "+":
+                    stack.append(num2 + num1)
+                elif word == "-":
+                    stack.append(num2 - num1)
+                elif word == "*":
+                    stack.append(num2 * num1)
+                else:
+                    if num1 * num2 < 0:
+                        stack.append(-((-num2) / num1))
+                    else:
+                        stack.append(num2 / num1)
+                
+        return stack.pop()
+'''
+Input:	["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output:	12
+Expected:	22
+'''
+# For python, 3 / (-5) = -1, which is ridiculous.
+        
