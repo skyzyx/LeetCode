e71e03f3fd79ea34e76a45262beb5691a2ed090b
Jie Zhang <jiez@ucsc.edu>
2015-05-10 11:59:16 -0700

    Combination sum, each number can be used for unlimited times.

--- 39. Combination Sum/Solution.cpp
+++ 39. Combination Sum/Solution.cpp
@@ -8,27 +8,27 @@ public:
         sort(candidates.begin(), candidates.end());
         
         vector<int> cur;
-        recursiveCombination(candidates, 0, target, cur, res);
+        dfs(candidates, 0, target, cur, res);
         return res;
     }
     
 private:
-    void recursiveCombination(vector<int> &candidates, int start, int target, vector<int> &cur, vector<vector<int>> &res) {
+    void dfs(vector<int> &candidates, int start, int target, vector<int> &cur, vector<vector<int>> &res) {
         if (target == 0) {
-            res.push_back(cur);
+            if (find(res.begin(), res.end(), cur) == res.end())
+                res.push_back(cur);
             return;
         }
         
         for (int i = start; i < candidates.size(); i++) {
-            if (i > start && candidates[i] == candidates[i-1])
-                continue;
+            //if (i > start && candidates[i] == candidates[i-1])
+                //continue;
             
             if (target >= candidates[i]) {
                 cur.push_back(candidates[i]);
-                recursiveCombination(candidates, i, target-candidates[i], cur, res);
+                dfs(candidates, i, target-candidates[i], cur, res);
                 cur.pop_back();
             }
         }
-            
     }
 };
