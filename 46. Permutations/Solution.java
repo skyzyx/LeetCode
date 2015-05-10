7bf93c284273bc599356d24ae3de0d2f13dcc136
Jie Zhang <jiez@ucsc.edu>
2015-05-10 13:24:44 -0700

    Given a collection of numbers, return all possible permutations.

--- 46. Permutations/Solution.java
+++ 46. Permutations/Solution.java
@@ -1,3 +1,5 @@
+// Permutations of a word, http://www.ardendertat.com/2011/10/28/programming-interview-questions-11-all-permutations-of-string/
+
 public class Solution {
     public ArrayList<ArrayList<Integer>> permute(int[] num) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
@@ -17,12 +19,46 @@ public class Solution {
         }
         
         for (int i = 0; i < num.length; i++) {
-            if (cur.contains(num[i]))
-                continue;
+            if (!cur.contains(num[i])) {
+                cur.add(num[i]);
+                dfs(num, cur, res);
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+}
+
+/* 
+Input:  [0,1]
+Output: [[0,1]]
+Expected:   [[0,1],[1,0]]
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (num == null || num.length == 0)
+            return res;
+            
+        Arrays.sort(num);
+        ArrayList<Integer> cur = new ArrayList<Integer>();
+        dfs(0, num, cur, res);
+        return res;
+    }
+    
+    private void dfs(int start, int[] num, ArrayList<Integer> cur, ArrayList<ArrayList<Integer>> res) {
+        if (cur.size() == num.length) {
+            res.add(new ArrayList<Integer>(cur));
+            return;
+        }
+        
+        for (int i = start; i < num.length; i++) {
+            //if (cur.contains(num[i]))
+                //continue;
                 
             cur.add(num[i]);
-            dfs(num, cur, res);
+            dfs(i+1, num, cur, res);
             cur.remove(cur.size() - 1);
         }
     }
 }
+*/
