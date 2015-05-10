5015c773436d0c9912947c6a5e26a2c6a8187a2d
Jie Zhang <jiez@ucsc.edu>
2015-05-09 18:49:28 -0700

    Binary tree preorder traversal.

---
+++ 144. Binary Tree Preorder Traversal/Solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> res;
+        if (root == NULL)
+            return res;
+            
+        stack<TreeNode*> s;
+        s.push(root);
+        
+        while (!s.empty()) {
+            TreeNode* tmp = s.top();
+            s.pop();
+            
+            res.push_back(tmp->val);
+            
+            if (tmp->right != NULL)
+                s.push(tmp->right);
+            if (tmp->left != NULL)
+                s.push(tmp->left);
+        }
+        return res;
+    }
+};
