420f54eadd0f883bfa19dba4f279708244588261
Jie Zhang <jiez@ucsc.edu>
2015-05-10 11:50:40 -0700

    Combination sum, each number may only be used once.

--- 40. Combination Sum II/Solution.cpp
+++ 40. Combination Sum II/Solution.cpp
@@ -15,13 +15,19 @@ public:
 private:
     void recursiveCombination(vector<int> &num, int start, int target, vector<int> &cur, vector<vector<int>> &res) {
         if (target == 0) {
-            res.push_back(cur);
+            if (find(res.begin(), res.end(), cur) == res.end())
+                res.push_back(cur);
             return;
         }
         
         for (int i = start; i < num.size(); i++) {
-            if (i > start && num[i] == num[i-1])
-                continue;
+            //if (i > start && num[i] == num[i-1])
+                //continue;
+            /*    
+            Input:  [1,1], 1
+            Output: [[1],[1]]
+            Expected:   [[1]]
+            */
             if (target >= num[i]) {
                 cur.push_back(num[i]);
                 recursiveCombination(num, i+1, target-num[i], cur, res);
