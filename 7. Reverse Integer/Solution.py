71162cb454d72f3a46a9997b47aefcbc68fa577c
Jie Zhang <jiez@ucsc.edu>
2015-05-11 12:18:20 -0700

    Reverse digits of an integer, 123 to 321, -123 to -321.

--- 7. Reverse Integer/Solution.py
+++ 7. Reverse Integer/Solution.py
@@ -2,16 +2,19 @@ class Solution:
     # @param {integer} x
     # @return {integer}
     def reverse(self, x):
-        sign = 1 if x >= 0 else -1
-        x = abs(x)
+        sign = 1
+        if x < 0:
+            sign = -1
+            
+        x = abs(x)    
         res = 0
         
         while x != 0:
             res = res * 10 + x % 10
             x /= 10
-           
+            
         res = res * sign
-        if res > math.pow(2, 31) - 1 or res < math.pow(2, 31) * (-1):
+        if res > (1 << 31) - 1 or res < -(1 << 31): #if res > math.pow(2, 31) - 1 or res < math.pow(2, 31) * (-1):
             return 0
         else:
             return res
