dab793003213390fa47416b1bee272d79abc9ce2
Jie Zhang <jiez@ucsc.edu>
2015-05-10 23:29:50 -0700

    Determine whether an integer is a palindrome.

---
+++ 9. Palindrome Number/Solution.c
@@ -0,0 +1,20 @@
+bool isPalindrome(int x) {
+    // Input:	-2147483648
+    // Output:	true
+    // Expected:	false 
+    if (x < 0)
+        return false;
+            
+    int div = 1;
+    while (div <= x / 10)
+        div *= 10;
+            
+    while (x > 0) {
+        if (x / div != x % 10)
+            return false;
+                
+        x = (x % div) / 10;
+        div /= 100;
+    }
+    return true;
+}
