d93fd3d879e7e4abc0687b44f46e15f338d0a6c7
Jie Zhang <jiez@ucsc.edu>
2015-05-13 14:37:21 -0700

    Insertion sort list.

---
+++ 147. Insertion Sort List/Solution.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def insertionSortList(self, head):
+        if head == None or head.next == None:
+            return head
+            
+        cur = head.next
+        while cur != None:
+            tmp = head
+            while cur != tmp:
+                if cur.val < tmp.val:
+                    data = cur.val
+                    cur.val = tmp.val
+                    tmp.val = data
+                
+                tmp = tmp.next
+            
+            cur = cur.next
+        return head
