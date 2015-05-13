676701abb79676392c75b0f47fdfcbdbeebf6721
Jie Zhang <jiez@ucsc.edu>
2015-05-13 14:36:56 -0700

    Insertion sort list.

---
+++ 147. Insertion Sort List/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+            
+        ListNode cur = head.next;
+        while (cur != null) {
+            ListNode tmp = head;
+            while (cur != tmp) {
+                if (cur.val < tmp.val) {
+                    int data = cur.val;
+                    cur.val = tmp.val;
+                    tmp.val = data;
+                }
+                tmp = tmp.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
