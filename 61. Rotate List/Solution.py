da29cbd6f5f4a7343f4c59db483a2efbac1e95b5
Jie Zhang <jiez@ucsc.edu>
2015-05-13 10:11:47 -0700

    Rotate a list to the right by k places.

---
+++ 61. Rotate List/Solution.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @param {integer} k
+    # @return {ListNode}
+    def rotateRight(self, head, k):
+        if head == None or k < 1:
+            return head
+            
+        size = 1
+        p = head
+        while p.next != None:
+            p = p.next
+            size += 1
+            
+        p.next = head
+        
+        step = size - k % size
+        while step > 0:
+            p = p.next
+            step -= 1
+            
+        head = p.next
+        p.next = None
+        return head
