398c728b273055aa48d48e2d6d280e7a798816e3
Jie Zhang <jiez@ucsc.edu>
2015-05-13 14:37:43 -0700

    Insertion sort list.

---
+++ 147. Insertion Sort List/Solution.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* insertionSortList(struct ListNode* head) {
+    if (head == NULL || head->next == NULL)
+        return head;
+            
+    struct ListNode* cur = head->next;
+    while (cur != NULL) {
+        struct ListNode* tmp = head;
+        while (tmp != cur) {
+            if (tmp->val > cur->val) {
+            int data = cur->val;
+                cur->val = tmp->val;
+                tmp->val = data;
+            }
+            tmp = tmp->next;
+        }
+        cur = cur->next;
+    }
+    return head;
+}
