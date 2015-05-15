4112eb6f8c440624f5a1d833ab2cda09b0acdc54
Jie Zhang <jiez@ucsc.edu>
2015-05-15 11:46:42 -0700

    Merge two sorted lists.

--- 21. Merge Two Sorted Lists/Solution.c
+++ 21. Merge Two Sorted Lists/Solution.c
@@ -5,6 +5,35 @@
  *     struct ListNode *next;
  * };
  */
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* dummyHead = malloc(sizeof(struct ListNode));
+    dummyHead->val = INT_MIN;
+    dummyHead->next = l1;
+    
+    struct ListNode* pre = dummyHead;
+    
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val <= l2->val)
+            l1 = l1->next;
+        else {
+            struct ListNode* l2next = l2->next;
+            pre->next = l2;
+            l2->next = l1;
+            
+            l2 = l2next;
+        }
+        
+        pre = pre->next;
+    }
+    
+    if (l2 != NULL)
+        pre->next = l2;
+        
+    return dummyHead->next;
+} 
+
+/*
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
     if (l1 == NULL)
         return l2;
@@ -34,3 +63,4 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
         
     return dummyHead->next;
 }
+*/
