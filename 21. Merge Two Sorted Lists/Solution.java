b35c417d4793176b98818408ec0f1ca1b6acda0b
Jie Zhang <jiez@ucsc.edu>
2015-05-15 11:45:49 -0700

    Merge two sorted lists.

--- 21. Merge Two Sorted Lists/Solution.java
+++ 21. Merge Two Sorted Lists/Solution.java
@@ -9,6 +9,37 @@
  *     }
  * }
  */
+ 
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
+        dummyHead.next = l1;
+        
+        ListNode pre = dummyHead;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val)
+                l1 = l1.next;
+            else {
+                ListNode l2next = l2.next;
+                l2.next = l1;
+                pre.next = l2;
+                
+                l2 = l2next;
+            }
+            
+            pre = pre.next;
+        }
+        
+        if (l2 != null)
+            pre.next = l2;
+            
+        return dummyHead.next;
+    }
+}
+
+
+/* 
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null)
@@ -39,3 +70,4 @@ public class Solution {
             
     }
 }
+*/
