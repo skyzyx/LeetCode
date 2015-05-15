b7e8fe8b2b58f47fb75807e3c2c09b325f2f17c7
Jie Zhang <jiez@ucsc.edu>
2015-05-15 11:45:16 -0700

    Merge two sorted lists.

--- 21. Merge Two Sorted Lists/Solution.cpp
+++ 21. Merge Two Sorted Lists/Solution.cpp
@@ -6,6 +6,37 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(INT_MIN);
+        dummyHead->next = l1;
+        
+        ListNode* pre = dummyHead;
+        
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val < l2->val)
+                l1 = l1->next;
+            else {
+                ListNode* l2next = l2->next;
+                pre->next = l2;
+                l2->next = l1;
+                
+                l2 = l2next;
+            }
+            
+            pre = pre->next;
+        }
+        
+        if (l2 != NULL)
+            pre->next = l2;
+            
+        return dummyHead->next;
+    }
+};
+ 
+/* 
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
@@ -36,3 +67,4 @@ public:
         return dummyHead->next;
     }
 };
+*/
