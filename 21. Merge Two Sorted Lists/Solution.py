0a575097e4e6a031931865e2ee108bcde5908546
Jie Zhang <jiez@ucsc.edu>
2015-05-15 22:07:13 -0700

    Merge two sorted lists.

--- 21. Merge Two Sorted Lists/Solution.py
+++ 21. Merge Two Sorted Lists/Solution.py
@@ -27,34 +27,3 @@ class Solution:
             pre.next = l2
             
         return dummyHead.next
-
-'''
-class Solution:
-    # @param two ListNodes
-    # @return a ListNode
-    def mergeTwoLists(self, l1, l2):
-        if l1 == None:
-            return l2
-        if l2 == None:
-            return l1
-            
-        dummyHead = ListNode(-1)
-        p = dummyHead
-        
-        while l1 != None and l2 != None:
-            if l1.val >= l2.val:
-                p.next = l2
-                l2 = l2.next
-            else:
-                p.next = l1
-                l1 = l1.next
-            
-            p = p.next
-            
-        if l1 != None:
-            p.next = l1
-        if l2 != None:
-            p.next = l2
-            
-        return dummyHead.next
-'''
