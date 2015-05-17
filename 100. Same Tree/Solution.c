4daa701cfda209d4eda82ee9c1ce47d3a17c7adc
Jie Zhang <jiez@ucsc.edu>
2015-05-17 13:09:08 -0700

    Same tree.

--- 100. Same Tree/Solution.c
+++ 100. Same Tree/Solution.c
@@ -9,8 +9,7 @@
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
     if (p == NULL && q == NULL)
         return true;
-    else if (p != NULL && q != NULL && p->val == q->val)
+    if (p != NULL && q != NULL && p->val == q->val)
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-    else
-        return false;
+    return false;
 }
