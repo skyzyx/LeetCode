bad368adc6c507195f9ea26df0f602a99d15ddf8
Jie Zhang <jiez@ucsc.edu>
2015-05-17 13:19:37 -0700

    Symmetric tree.

--- 101. Symmetric Tree/Solution.c
+++ 101. Symmetric Tree/Solution.c
@@ -9,16 +9,14 @@
 bool dfs(struct TreeNode* p, struct TreeNode* q) {
     if (p == NULL && q == NULL)
         return true;
-    if (p == NULL || q == NULL)
-        return false;
-    if (p->val != q->val)
-        return false;
-    return dfs(p->left, q->right) && dfs(p->right, q->left);
+    if (p != NULL && q != NULL && p->val == q->val)
+        return dfs(p->left, q->right) && dfs(p->right, q->left);
+    return false;
 }
+
 
 bool isSymmetric(struct TreeNode* root) {
     if (root == NULL)
         return true;
     return dfs(root->left, root->right);
 }
-
