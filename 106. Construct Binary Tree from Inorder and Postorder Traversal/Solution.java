6c07a3dd50c07f9ef38ace2ac851f7250bc6c3df
Jie Zhang <jiez@ucsc.edu>
2015-05-16 20:41:32 -0700

    Postorder and inorder.

--- 106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -11,26 +11,22 @@ public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if (inorder == null || postorder == null)
             return null;
-        int postLen = postorder.length - 1;
-        int inLen = inorder.length - 1;
-        return buildTree(inorder, 0, inLen, postorder, 0, postLen);
+            
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+        return dfs(map, inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
     
-    private TreeNode buildTree(int[] inorder, int inL, int inR, int[] postorder, int postL, int postR) {
-        if (inL > inR)
+    private TreeNode dfs(HashMap<Integer, Integer> map, int[] inorder, int inL, int inR, int[] postorder, int posL, int posR) {
+        if (inL > inR || posL > posR)
             return null;
-        TreeNode root = new TreeNode(postorder[postR]);
-        int rootIndex = 0;
-        for (int i = 0; i < inorder.length; i++) {
-            if (root.val == inorder[i]) {
-                rootIndex = i;
-                break;
-            }
-        }
+            
+        TreeNode root = new TreeNode(postorder[posR]);
+        int rootIndex = map.get(postorder[posR]);
         
-        int size = rootIndex - inL;
-        root.left = buildTree(inorder, inL, rootIndex - 1, postorder, postL, postL + size - 1);
-        root.right = buildTree(inorder, rootIndex + 1, inR, postorder, postL + size, postR - 1);
+        root.left = dfs(map, inorder, inL, rootIndex - 1, postorder, posL, posL+rootIndex-inL-1);
+        root.right = dfs(map, inorder, rootIndex+1, inR, postorder, posR+rootIndex-inR, posR-1);
         return root;
     }
 }
