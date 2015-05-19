533a8e2ce925caaaa0af45b4e6eeb50af97894d7
Jie Zhang <jiez@ucsc.edu>
2015-05-18 19:47:33 -0700

    Find all possible subsets, duplicate numbers included.

--- 90. Subsets II/Solution.java
+++ 90. Subsets II/Solution.java
@@ -3,6 +3,29 @@ public class Solution {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
         if (num == null || num.length == 0)
             return res;
+            
+        Arrays.sort(num);
+        res.add(new ArrayList<Integer>());
+        
+        for (int i = 0; i < num.length; i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                ArrayList<Integer> cur = new ArrayList<Integer>(res.get(j));
+                cur.add(num[i]);
+                if (!res.contains(cur))
+                    res.add(cur);
+            }
+        }
+        return res;
+    }
+}
+
+/*
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (num == null || num.length == 0)
+            return res;
         
         Arrays.sort(num);
         int size = 1 << num.length;
@@ -23,3 +46,4 @@ public class Solution {
         return res;
     }
 }
+*/
