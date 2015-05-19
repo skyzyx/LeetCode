dd0c811ac73012c6bb9ffda6e787959722269389
Jie Zhang <jiez@ucsc.edu>
2015-05-18 19:47:46 -0700

    Find all possible subsets, duplicate numbers included.

--- 90. Subsets II/Solution.cpp
+++ 90. Subsets II/Solution.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    vector<vector<int>> subsetsWithDup(vector<int> &S) {
+        vector<vector<int>> res;
+        vector<int> tmp;
+        res.push_back(tmp);
+        if (S.size() == 0)
+            return res;
+            
+        sort(S.begin(), S.end());
+        
+        for (int i = 0; i < S.size(); i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                vector<int> cur = res[j];
+                cur.push_back(S[i]);
+                if (find(res.begin(), res.end(), cur) == res.end())
+                    res.push_back(cur);
+            }
+        }
+        return res;
+    }
+};
+
+/*
+class Solution {
+public:
     vector<vector<int> > subsetsWithDup(vector<int> &S) {
         vector<vector<int>> res (1);
         sort(S.begin(), S.end());
@@ -22,3 +47,4 @@ public:
         return res;
     }
 };
+*/
