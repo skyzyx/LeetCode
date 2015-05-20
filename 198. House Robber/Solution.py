ab670f58381cf585c8be452714ab0e1cd4ad2497
Jie Zhang <jiez@ucsc.edu>
2015-05-20 16:26:55 -0700

    House robber.

--- 198. House Robber/Solution.py
+++ 198. House Robber/Solution.py
@@ -1,4 +1,17 @@
 class Solution:
+    def rob(self, nums):
+        size = len(nums)
+        even = 0
+        odd = 0
+        for i in xrange(size):
+            if i % 2 == 0:
+                even = max(even + nums[i], odd)
+            else:
+                odd = max(odd + nums[i], even)
+        return max(even, odd)
+
+'''
+class Solution:
     # @param {integer[]} nums
     # @return {integer}
     def rob(self, nums):
@@ -12,4 +25,5 @@ class Solution:
             dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
             
         return dp[len(nums)]
+'''
         
