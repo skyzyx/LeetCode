75ab6da541383e2ec3f8a7ec015340a30a953c4a
Jie Zhang <jiez@ucsc.edu>
2015-05-20 16:26:37 -0700

    House robber.

--- 198. House Robber/Solution.java
+++ 198. House Robber/Solution.java
@@ -1,15 +1,13 @@
-
 public class Solution {
-    public int rob(int[] num) {
-        if (num == null || num.length == 0)
-            return 0;
-            
-        int[] dp = new int[num.length+1];
-        dp[0] = 0;
-        dp[1] = num[0];
-        for (int i = 2; i <= num.length; i++) {
-            dp[i] = Math.max(dp[i-1], dp[i-2] + num[i-1]);
+    public int rob(int[] nums) {
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0)
+                even = Math.max(even + nums[i], odd);
+            else
+                odd = Math.max(odd + nums[i], even);
         }
-        return dp[num.length];
+        return Math.max(even, odd);
     }
 }
