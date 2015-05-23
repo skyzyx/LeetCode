a938572cdb0101ed04d652492a5a220c0900c5e6
Jie Zhang <jiez@ucsc.edu>
2015-05-22 22:12:59 -0700

    Given an unsorted integer array, find the first missing positive integer.

---
+++ 41. First Missing Positive/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i+1 && nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1])
+                swap(nums, i, nums[i]-1);
+            else
+                i++;
+        }
+        
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1)
+                return j+1;
+        }
+        return nums.length+1;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
