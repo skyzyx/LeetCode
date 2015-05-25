546c2dadc96a986f07ccb984a65d67e867474ae0
Jie Zhang <jiez@ucsc.edu>
2015-05-24 22:00:05 -0700

    Find if the array contains duplicates.

--- 217. Constains Duplicate/Solution.cpp
+++ 217. Constains Duplicate/Solution.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    bool containsDuplicate(vector<int> &nums) {
+        if (nums.size() < 2)
+            return false;
+            
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map[nums[i]] == 1)
+                return true;
+            else
+                map[nums[i]] = 1;
+        }
+        return false;
+    }
+};
+
+/*
+class Solution {
+public:
     bool containsDuplicate(vector<int>& nums) {
         if (nums.size() < 2)
             return false;
@@ -12,3 +30,4 @@ public:
         return false;
     }
 };
+*/
