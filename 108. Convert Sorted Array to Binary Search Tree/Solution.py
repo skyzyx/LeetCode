3852e074fda0d68b04691e3544b1dce4cce6c522
Jie Zhang <jiez@ucsc.edu>
2015-05-30 14:21:42 -0700

    Convert sorted array to BST.

--- 108. Convert Sorted Array to Binary Search Tree/Solution.py
+++ 108. Convert Sorted Array to Binary Search Tree/Solution.py
@@ -1,3 +1,7 @@
+# LeetCode #108: Convert Sorted Array to Binary Search Tree
+
+# Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
 # Definition for a  binary tree node
 # class TreeNode:
 #     def __init__(self, x):
