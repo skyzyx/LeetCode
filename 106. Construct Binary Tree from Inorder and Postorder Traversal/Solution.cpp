c364fa09f89ade95f196f204e08a24d7799df60e
Jie Zhang <jiez@ucsc.edu>
2015-05-30 14:15:20 -0700

    Construct binary tree from inorder and postorder.

--- 106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.cpp
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.cpp
@@ -1,3 +1,12 @@
+// LeetCode #106: Construct Binary Tree from Inorder and Postorder Traversal
+
+/*
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
