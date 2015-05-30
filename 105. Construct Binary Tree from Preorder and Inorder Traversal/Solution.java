222a16ed05c66db3b996523efe8c49192bb584dd
Jie Zhang <jiez@ucsc.edu>
2015-05-30 14:11:59 -0700

    Construct binary tree from preorder and inorder.

--- 105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
@@ -1,3 +1,12 @@
+// LeetCode #105: Construct Binary Tree from Preorder and Inorder Traversal 
+
+/*
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+*/
+
 /**
  * Definition for binary tree
  * public class TreeNode {
--- 105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.py
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.py
@@ -1,3 +1,12 @@
+# LeetCode #105: Construct Binary Tree from Preorder and Inorder Traversal 
+
+'''
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
