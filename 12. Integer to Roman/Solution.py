2f1a05b9681865b8ce6b26b24a976a910dd9fa21
Jie Zhang <jiez@ucsc.edu>
2015-06-01 11:21:40 -0700

    Integer to Roman.

--- 12. Integer to Roman/Solution.py
+++ 12. Integer to Roman/Solution.py
@@ -1,3 +1,11 @@
+# LeetCode #12: Integer to Roman
+
+'''
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+'''
+
 class Solution:
     # @return a string
     def intToRoman(self, num):
