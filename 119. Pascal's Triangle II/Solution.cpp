52cfe35de16d67abe1f023fca57e5052399dd11b
Jie Zhang <jiez@ucsc.edu>
2015-06-01 09:54:46 -0700

    Given an index k, return the kth row of the Pascal's triangle.

--- 119. Pascal's Triangle II/Solution.cpp
+++ 119. Pascal's Triangle II/Solution.cpp
@@ -1,3 +1,15 @@
+// LeetCode #119: Pascal's Triangle II
+
+/*
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+*/
+
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
