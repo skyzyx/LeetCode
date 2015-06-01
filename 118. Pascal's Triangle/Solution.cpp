dde0d24b57955879200679ae60cdac85b47db2a2
Jie Zhang <jiez@ucsc.edu>
2015-06-01 09:51:31 -0700

    Given numRows, generate the Pascal's triangle.

--- 118. Pascal's Triangle/Solution.cpp
+++ 118. Pascal's Triangle/Solution.cpp
@@ -1,5 +1,19 @@
-// https://helloacm.com/c-coding-exercise-pascals-triangle/
+// LeetCode #118: Pascal's Triangle
 
+/*
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
 class Solution {
 public:
     vector<vector<int> > generate(int numRows) {
