d64444f8ef56677e7a3058c4a73f4dc7b8ce47e1
Jie Zhang <jiez@ucsc.edu>
2015-06-01 11:21:15 -0700

    Integer to Roman.

--- 12. Integer to Roman/Solution.java
+++ 12. Integer to Roman/Solution.java
@@ -1,3 +1,11 @@
+// LeetCode #12: Integer to Roman
+
+/*
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
 public class Solution {
     public String intToRoman(int num) {
         
