8f83b64f6331d0ef69173c9d791c5ac2d1ed7a5b
Jie Zhang <jiez@ucsc.edu>
2015-06-01 11:24:31 -0700

    At most one transaction.

--- 121. Best Time to Buy and Sell Stock/Solution.java
+++ 121. Best Time to Buy and Sell Stock/Solution.java
@@ -1,3 +1,11 @@
+// LeetCode #121: Best Time to Buy and Sell Stock
+
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+*/
+
 public class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length < 2)
