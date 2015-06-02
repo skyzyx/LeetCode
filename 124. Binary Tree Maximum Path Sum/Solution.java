c0af5e72dc9435257595b256d9ad5b18f941973a
Jie Zhang <jiez@ucsc.edu>
2015-06-02 11:15:06 -0700

    Find the maximum path sum of a binary tree, the path may start and end at any node.

--- 124. Binary Tree Maximum Path Sum/Solution.java
+++ 124. Binary Tree Maximum Path Sum/Solution.java
@@ -1,3 +1,19 @@
+// LeetCode #124: Binary Tree Maximum Path Sum
+
+/*
+Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+*/
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
