4d1dbc453085142bc8fedeb4c03c833429061317
Jie Zhang <jiez@ucsc.edu>
2015-06-02 11:30:45 -0700

    Roman to integer.

--- 13. Roman to Integer/Solution.cpp
+++ 13. Roman to Integer/Solution.cpp
@@ -1,3 +1,11 @@
+// LeetCode #13: Roman to Integer
+
+/*
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
 class Solution {
 public:
     
