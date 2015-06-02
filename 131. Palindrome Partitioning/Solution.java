0969f697d0deac639a5f717fa3d35b02da97aae5
Jie Zhang <jiez@ucsc.edu>
2015-06-02 11:36:40 -0700

    Partition a string such that every substring of the partition is a palindrome.

--- 131. Palindrome Partitioning/Solution.java
+++ 131. Palindrome Partitioning/Solution.java
@@ -1,3 +1,19 @@
+// LeetCode #131: Palindrome Partitioning
+
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+  [
+    ["aa","b"],
+    ["a","a","b"]
+  ]
+*/
+
 public class Solution {
     public ArrayList<ArrayList<String>> partition(String s) {
         ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
