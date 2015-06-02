053d94c0e8a04809461d101a881ee431ce168bd8
Jie Zhang <jiez@ucsc.edu>
2015-06-02 11:36:23 -0700

    Partition a string such that every substring of the partition is a palindrome.

--- 131. Palindrome Partitioning/Solution.cpp
+++ 131. Palindrome Partitioning/Solution.cpp
@@ -1,3 +1,19 @@
+// LeetCode #131: Palindrome Partitioning
+
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+  [
+    ["aa","b"],
+    ["a","a","b"]
+  ]
+*/
+
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
