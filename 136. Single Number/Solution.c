4abadf79132da4a12ac39a935bdeaee6b4646bcd
Jie Zhang <jiez@ucsc.edu>
2015-06-03 10:00:48 -0700

    Every element appears twice except for one, find that one.

--- 136. Single Number/Solution.c
+++ 136. Single Number/Solution.c
@@ -1,3 +1,12 @@
+// LeetCode #136: Single Number
+
+/*
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
 int singleNumber(int A[], int n) {
     int res = 0;
     for (int i = 0; i < n; i++)
