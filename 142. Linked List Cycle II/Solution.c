848834ce5207117b801efff979627bef853b6e48
Jie Zhang <jiez@ucsc.edu>
2015-06-03 10:14:46 -0700

    Return the node where the cycle begins.

--- 142. Linked List Cycle II/Solution.c
+++ 142. Linked List Cycle II/Solution.c
@@ -1,3 +1,12 @@
+// LeetCode #142: Linked List Cycle II
+
+/*
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
