8d57f016812861eefec08a66ae041590a2a3b676
Jie Zhang <jiez@ucsc.edu>
2015-06-03 10:11:04 -0700

    Determine if a linked list has a cycle.

--- 141. Linked List Cycle/Solution.c
+++ 141. Linked List Cycle/Solution.c
@@ -1,3 +1,12 @@
+// LeetCode #141: Linked List Cycle
+
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
