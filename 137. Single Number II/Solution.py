f39fe944102e198c012e78eeb02ec0fc422fd1de
Jie Zhang <jiez@ucsc.edu>
2015-06-03 10:04:29 -0700

    Every element appears three times except one, find that one.

--- 137. Single Number II/Solution.py
+++ 137. Single Number II/Solution.py
@@ -1,3 +1,12 @@
+# LeetCode #137: Single Number II
+
+'''
+Given an array of integers, every element appears three times except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+'''
+
 class Solution:
     # @param A, a list of integer
     # @return an integer
