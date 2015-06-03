903ceff4206aedde51257216e2893537608e5333
Jie Zhang <jiez@ucsc.edu>
2015-06-03 10:03:12 -0700

    Every element appears three times except one, find that one.

--- 137. Single Number II/Solution.cpp
+++ 137. Single Number II/Solution.cpp
@@ -1,3 +1,12 @@
+// LeetCode #137: Single Number II
+
+/*
+Given an array of integers, every element appears three times except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
