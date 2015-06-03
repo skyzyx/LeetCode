03cf8bedde4b680ce2526b5d74e856c7c9950cb2
Jie Zhang <jiez@ucsc.edu>
2015-06-03 10:06:41 -0700

    Longest common prefix.

--- 14. Longest Common Prefix/Solution.cpp
+++ 14. Longest Common Prefix/Solution.cpp
@@ -1,3 +1,9 @@
+// LeetCode #14: Longest Common Prefix
+
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+*/
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string> &strs) {
