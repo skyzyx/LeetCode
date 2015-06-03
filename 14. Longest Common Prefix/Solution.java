ce0a7259d77ff04edda9a5e9b3b275c5f65c0684
Jie Zhang <jiez@ucsc.edu>
2015-06-03 10:07:04 -0700

    Longest common prefix.

--- 14. Longest Common Prefix/Solution.java
+++ 14. Longest Common Prefix/Solution.java
@@ -1,3 +1,9 @@
+// LeetCode #14: Longest Common Prefix
+
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+*/
+
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0)
