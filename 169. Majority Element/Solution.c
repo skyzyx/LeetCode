91cc7d81acb9a49098b0d8e13b0a14b7d46c7236
Jie Zhang <jiez@ucsc.edu>
2015-06-05 10:10:38 -0700

    Find the majority element.

--- 169. Majority Element/Solution.c
+++ 169. Majority Element/Solution.c
@@ -1,3 +1,11 @@
+// LeetCode #169: Majority Element
+
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+*/
+
 int majorityElement(int* nums, int numsSize) {
     if (nums == NULL || numsSize == 0)
         return 0;
