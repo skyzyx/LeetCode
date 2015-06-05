3493ec5be28c85ac178651fcd508de3d97ab97d2
Jie Zhang <jiez@ucsc.edu>
2015-06-04 21:41:12 -0700

    Find the contiguous subarray within an array (containing at least one number) which has the largest product.

--- 152. Maximum Product Subarray/Solution.py
+++ 152. Maximum Product Subarray/Solution.py
@@ -1,3 +1,12 @@
+# LeetCode #152: Maximum Product Subarray
+
+'''
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6.
+'''
+
 class Solution:
     # @param A, a list of integers
     # @return an integer
