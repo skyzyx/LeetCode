336bc58de16fecedec01fbf598ab4664f3098012
Jie Zhang <jiez@ucsc.edu>
2015-06-05 10:10:21 -0700

    Find the majority element.

--- 169. Majority Element/Solution.java
+++ 169. Majority Element/Solution.java
@@ -1,3 +1,11 @@
+// LeetCode #169: Majority Element
+
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+*/
+
 public class Solution {
     public int majorityElement(int[] num) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
