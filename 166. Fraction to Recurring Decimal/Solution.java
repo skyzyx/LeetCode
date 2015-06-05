6625fa32b642194c8c3ce365166d440e9f706b2e
Jie Zhang <jiez@ucsc.edu>
2015-06-05 10:02:42 -0700

    Fraction to recurring decimal.

--- 166. Fraction to Recurring Decimal/Solution.java
+++ 166. Fraction to Recurring Decimal/Solution.java
@@ -1,3 +1,17 @@
+// LeetCode #166: Fraction to Recurring Decimal
+
+/*
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+For example,
+
+Given numerator = 1, denominator = 2, return "0.5".
+Given numerator = 2, denominator = 1, return "2".
+Given numerator = 2, denominator = 3, return "0.(6)".
+*/
+
 public class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
         if (numerator == 0)
