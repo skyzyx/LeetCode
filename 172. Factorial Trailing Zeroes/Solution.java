daee4d9131e81b32681aed62cf6e54a0ab3db80c
Jie Zhang <jiez@ucsc.edu>
2015-06-05 10:18:34 -0700

    Return the number of trailing zeroes in n factorial.

--- 172. Factorial Trailing Zeroes/Solution.java
+++ 172. Factorial Trailing Zeroes/Solution.java
@@ -1,3 +1,11 @@
+// LeetCode #172: Factorial Trailing Zeroes
+
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+*/
+
 public class Solution {
     public int trailingZeroes(int n) {
         int count = 0;
