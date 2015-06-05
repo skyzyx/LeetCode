23bdc9f32aec6046a86ec38413506981472d358c
Jie Zhang <jiez@ucsc.edu>
2015-06-04 21:40:22 -0700

    Find the contiguous subarray within an array (containing at least one number) which has the largest product.

--- 152. Maximum Product Subarray/Solution.java
+++ 152. Maximum Product Subarray/Solution.java
@@ -1,3 +1,12 @@
+// LeetCode #152: Maximum Product Subarray
+
+/*
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6.
+*/
+
 public class Solution {
     public int maxProduct(int[] A) {
         int maxPro = A[0];
