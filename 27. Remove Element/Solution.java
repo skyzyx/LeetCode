0b95479c35e284945245a75e39c652c9d087c4af
Jie Zhang <jiez@ucsc.edu>
2015-06-07 21:39:44 -0700

    Remove all instances of that value in place and return the new length.

--- 27. Remove Element/Solution.java
+++ 27. Remove Element/Solution.java
@@ -1,3 +1,11 @@
+// LeetCode #27: Remove Element
+
+/*
+Given an array and a value, remove all instances of that value in place and return the new length.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
 public class Solution {
     public int removeElement(int[] A, int elem) {
         if (A == null)
