99d0d4c15ecb790ee3a536fff1bfa9e791685b04
Jie Zhang <jiez@ucsc.edu>
2015-06-07 21:34:42 -0700

    Implement strStr().

--- 28. Implement strStr()/Solution.java
+++ 28. Implement strStr()/Solution.java
@@ -1,3 +1,11 @@
+// LeetCode #28: Implement strStr()
+
+/*
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
 public class Solution {
     public int strStr(String haystack, String needle) {
         int hlen = haystack.length();
