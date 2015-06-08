11625526156d94a9ac2338f0b03d7ea5545d4a0c
Jie Zhang <jiez@ucsc.edu>
2015-06-08 10:30:54 -0700

    Climbing stairs.

--- 70. Climbing Stairs/Solution.py
+++ 70. Climbing Stairs/Solution.py
@@ -1,3 +1,11 @@
+# LeetCode #70: Climbing Stairs
+
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+'''
+
 class Solution:
     def climbStairs(self, n):
         dp = [1, 1];
