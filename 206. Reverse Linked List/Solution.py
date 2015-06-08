806c856884c38b74c43894673812ec6aadbe7a99
Jie Zhang <jiez@ucsc.edu>
2015-06-07 21:18:27 -0700

    Reverse linked list.

--- 206. Reverse Linked List/Solution.py
+++ 206. Reverse Linked List/Solution.py
@@ -1,3 +1,14 @@
+# LeetCode #206: Reverse Linked List
+
+'''
+Reverse a singly linked list.
+
+click to show more hints.
+
+Hint:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
