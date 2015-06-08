5875a4554b03b5536f6ccfc509e5b85e271f0fa8
Jie Zhang <jiez@ucsc.edu>
2015-06-07 21:35:33 -0700

    Implement strStr().

--- 28. Implement strStr()/Solution.py
+++ 28. Implement strStr()/Solution.py
@@ -1,3 +1,11 @@
+# LeetCode #28: Implement strStr()
+
+'''
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+'''
+
 class Solution:
     # @param {string} haystack
     # @param {string} needle
