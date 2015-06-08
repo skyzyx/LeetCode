d768f4f2c20dbc6e68877835daed4c1c10e79aeb
Jie Zhang <jiez@ucsc.edu>
2015-06-07 21:53:57 -0700

    Merge k sorted lists.

--- 23. Merge k Sorted Lists/Solution.cpp
+++ 23. Merge k Sorted Lists/Solution.cpp
@@ -1,3 +1,9 @@
+// LeetCode #23: Merge k Sorted Lists
+
+/*
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
