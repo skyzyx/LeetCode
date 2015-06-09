c390ad17e174a647ab7e9eb0bddb0ebcc96bf1ed
Jie Zhang <jiez@ucsc.edu>
2015-06-09 10:44:29 -0700

    Given a collection of integers that might contain duplicates, return all possible subsets.

--- 90. Subsets II/Solution.py
+++ 90. Subsets II/Solution.py
@@ -1,3 +1,24 @@
+# LeetCode #90: Subsets II
+
+'''
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If nums = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+'''
+
 class Solution:
     # @param num, a list of integer
     # @return a list of lists of integer
