b4020e56e949433942ad05c6c85aa9897f513e83
Jie Zhang <jiez@ucsc.edu>
2015-06-09 10:42:44 -0700

    Given a collection of integers that might contain duplicates, return all possible subsets.

--- 90. Subsets II/Solution.java
+++ 90. Subsets II/Solution.java
@@ -1,3 +1,24 @@
+// LeetCode #90: Subsets II
+
+/*
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If nums = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+*/
+
 public class Solution {
     public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
