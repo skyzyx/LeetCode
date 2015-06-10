5e9bcfa9c179d06537c1ff58d9498078cff7cbf1
Jie Zhang <jiez@ucsc.edu>
2015-06-10 10:34:39 -0700

    Implement a trie with insert, search, and startWith methods.

--- 208. Implement Trie (Prefix Tree)/Solution.cpp
+++ 208. Implement Trie (Prefix Tree)/Solution.cpp
@@ -1,3 +1,12 @@
+// LeetCode #208: Implement Trie (Prefix Tree)
+
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+*/
+
 class TrieNode {
 public:
     // Initialize your data structure here.
