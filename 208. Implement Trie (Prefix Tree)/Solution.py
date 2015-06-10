1333254ab0f2cba5c41e9cfbfbe78ead68d7eb8f
Jie Zhang <jiez@ucsc.edu>
2015-06-10 10:35:41 -0700

    Implement a trie with insert, search, and startWith methods.

--- 208. Implement Trie (Prefix Tree)/Solution.py
+++ 208. Implement Trie (Prefix Tree)/Solution.py
@@ -1,3 +1,12 @@
+# LeetCode #208: Implement Trie (Prefix Tree)
+
+'''
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+'''
+
 class TrieNode:
     # Initialize your data structure here.
     def __init__(self):
