71f3fd2b8436c5c2f378328fea826f01b85fecd7
Jie Zhang <jiez@ucsc.edu>
2015-06-12 22:41:58 -0700

    Letter combinations of a phone number.

--- 17. Letter Combinations of a Phone Number/Solution.cpp
+++ 17. Letter Combinations of a Phone Number/Solution.cpp
@@ -1,3 +1,14 @@
+// LeetCode #17: Letter Combination of a Phone Number
+
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+*/
+
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
