8cbce68aaa4803f6e0f047cd3a8f1928214784a9
Jie Zhang <jiez@ucsc.edu>
2015-06-12 22:51:37 -0700

    Find if an array contains any duplicates.

--- 217. Constains Duplicate/Solution.java
+++ 217. Constains Duplicate/Solution.java
@@ -1,3 +1,9 @@
+// LeetCode #217: Contains Duplicate
+
+/*
+Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+*/
+
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         if (nums == null || nums.length < 2)
