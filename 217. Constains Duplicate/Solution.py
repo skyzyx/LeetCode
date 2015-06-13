14a9727d2530b713c5719a5a00a5df88fc48cd24
Jie Zhang <jiez@ucsc.edu>
2015-06-12 22:52:13 -0700

    Find if an array contains any duplicates.

--- 217. Constains Duplicate/Solution.py
+++ 217. Constains Duplicate/Solution.py
@@ -1,3 +1,7 @@
+# LeetCode #217: Contains Duplicate
+
+# Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
 class Solution:
     def containsDuplicate(self, nums):
         if nums == None or len(nums) < 2:
