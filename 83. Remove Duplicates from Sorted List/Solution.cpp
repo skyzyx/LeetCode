1902c8dd04033166444b9b4a279678f14061431d
Jie Zhang <jiez@ucsc.edu>
2015-06-12 22:55:58 -0700

    Remove all duplicates from a list such that each element appears only once.

--- 83. Remove Duplicates from Sorted List/Solution.cpp
+++ 83. Remove Duplicates from Sorted List/Solution.cpp
@@ -1,3 +1,13 @@
+// LeetCode #83: Remove Duplicates from Sorted List
+
+/*
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
