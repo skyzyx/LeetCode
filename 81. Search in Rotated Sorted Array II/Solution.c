4d851e485967acb66f5d6c65011fa983dacbdabd
Jie Zhang <jiez@ucsc.edu>
2015-06-13 22:39:49 -0700

    Search in rotated sorted array, may contain duplicates.

--- 81. Search in Rotated Sorted Array II/Solution.c
+++ 81. Search in Rotated Sorted Array II/Solution.c
@@ -1,3 +1,14 @@
+// LeetCode #81: Search in Rotated Sorted Array II
+
+/*
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+*/
+
 bool search(int* nums, int numsSize, int target) {
     if (numsSize == 0)
             return false;
