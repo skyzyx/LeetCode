6fd473f67c352ca3e5033dce6fe79eb7f0d7e47d
Jie Zhang <jiez@ucsc.edu>
2015-06-13 22:39:35 -0700

    Search in rotated sorted array, may contain duplicates.

--- 81. Search in Rotated Sorted Array II/Solution.java
+++ 81. Search in Rotated Sorted Array II/Solution.java
@@ -1,3 +1,14 @@
+// LeetCode #81: Search in Rotated Sorted Array II
+
+/*
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+*/
+
 public class Solution {
     public boolean search(int[] nums, int target) {
         if (nums == null || nums.length == 0)
