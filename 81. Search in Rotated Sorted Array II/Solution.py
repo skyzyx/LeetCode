812117781b7be6e597d0c18f6086e1c708667266
Jie Zhang <jiez@ucsc.edu>
2015-06-13 22:40:13 -0700

    Search in rotated sorted array, may contain duplicates.

--- 81. Search in Rotated Sorted Array II/Solution.py
+++ 81. Search in Rotated Sorted Array II/Solution.py
@@ -1,3 +1,14 @@
+# LeetCode #81: Search in Rotated Sorted Array II
+
+'''
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+'''
+
 class Solution:
     # @param {integer[]} nums
     # @param {integer} target
