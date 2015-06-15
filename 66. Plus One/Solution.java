d3ca266f706cdb8d16febb289e0bc082e6a2bf58
Jie Zhang <jiez@ucsc.edu>
2015-06-14 23:23:24 -0700

    Given a non-negative number represented as an array of digits, plus one to the number.

--- 66. Plus One/Solution.java
+++ 66. Plus One/Solution.java
@@ -1,3 +1,11 @@
+// LeetCode #66: Plus One
+
+/*
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+*/
+
 public class Solution {
     public int[] plusOne(int[] digits) {
         if (digits == null || digits.length == 0)
