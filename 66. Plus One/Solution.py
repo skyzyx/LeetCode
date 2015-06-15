cc7f895105af81ca26da6d33b386c8dc075af058
Jie Zhang <jiez@ucsc.edu>
2015-06-14 23:24:30 -0700

    Given a non-negative number represented as an array of digits, plus one to the number.

--- 66. Plus One/Solution.py
+++ 66. Plus One/Solution.py
@@ -1,3 +1,11 @@
+# LeetCode #66: Plus One
+
+'''
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+'''
+
 class Solution:
     # @param digits, a list of integer digits
     # @return a list of integer digits
