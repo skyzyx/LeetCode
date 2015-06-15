866155845e884d68d8fd145168e4fc8549803ee0
Jie Zhang <jiez@ucsc.edu>
2015-06-14 23:27:41 -0700

    A robot is moving from upper left corner to bottom right corner of a grid, how many possible ways?

--- 62. Unique Paths/Solution.c
+++ 62. Unique Paths/Solution.c
@@ -1,3 +1,13 @@
+// LeetCode #62: Unique Paths
+
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+*/
+
 int uniquePaths(int m, int n) {
     int dp[m][n];
     for (int i = 0; i < n; i++)
