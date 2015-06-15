f24f04200c77eb4b81a13b3c56f69efe44b8e032
Jie Zhang <jiez@ucsc.edu>
2015-06-14 23:20:42 -0700

    Given two binary strings, return their sum (also a binary string).

--- 67. Add Binary/Solution.java
+++ 67. Add Binary/Solution.java
@@ -1,3 +1,14 @@
+// LeetCode #67: Add Binary
+
+/*
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+*/
+
 public class Solution {
     public String addBinary(String a, String b) {
         if (a == null || b == null)
