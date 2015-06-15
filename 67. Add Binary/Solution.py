4db4d22712b5336951689a0ee793c51d8553e805
Jie Zhang <jiez@ucsc.edu>
2015-06-14 23:21:08 -0700

    Given two binary strings, return their sum (also a binary string).

--- 67. Add Binary/Solution.py
+++ 67. Add Binary/Solution.py
@@ -1,3 +1,14 @@
+# LeetCode #67: Add Binary
+
+'''
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+'''
+
 class Solution:
     # @param {string} a
     # @param {string} b
