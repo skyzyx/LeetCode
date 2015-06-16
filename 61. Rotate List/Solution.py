3d67b0450b8cb69795e0a00de58a50ae3203b55f
Jie Zhang <jiez@ucsc.edu>
2015-06-15 22:34:09 -0700

    Rotate a list to the right by k places.

--- 61. Rotate List/Solution.py
+++ 61. Rotate List/Solution.py
@@ -1,3 +1,13 @@
+# LeetCode #61: Rotate List
+
+'''
+Given a list, rotate the list to the right by k places, where k is non-negative.
+
+For example:
+Given 1->2->3->4->5->NULL and k = 2,
+return 4->5->1->2->3->NULL.
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
