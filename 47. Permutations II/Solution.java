5129b250a1ffb974fc64a876ad6b248edf22e264
Jie Zhang <jiez@ucsc.edu>
2015-06-15 22:52:49 -0700

    Given a collection of numbers that might contain duplicates, return all possible unique permutations.

--- 47. Permutations II/Solution.java
+++ 47. Permutations II/Solution.java
@@ -1,3 +1,13 @@
+// LeetCode #47: Permutations II
+
+/*
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[1,1,2], [1,2,1], and [2,1,1].
+*/
+
 public class Solution {
     public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
