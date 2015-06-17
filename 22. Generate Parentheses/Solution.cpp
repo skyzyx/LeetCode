ce1498f7b853bc8cb656cb369ec302a4b324ce90
Jie Zhang <jiez@ucsc.edu>
2015-06-17 14:26:07 -0700

    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.

--- 22. Generate Parentheses/Solution.cpp
+++ 22. Generate Parentheses/Solution.cpp
@@ -1,3 +1,13 @@
+// LeetCode #22: Generate Parentheses
+
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+"((()))", "(()())", "(())()", "()(())", "()()()"
+*/
+
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
