994a3a23da54effb1975bb52c524a8a4eb0b7962
Jie Zhang <jiez@ucsc.edu>
2015-06-17 14:26:29 -0700

    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.

--- 22. Generate Parentheses/Solution.java
+++ 22. Generate Parentheses/Solution.java
@@ -1,3 +1,13 @@
+// LeetCode #22: Generate Parentheses
+
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+"((()))", "(()())", "(())()", "()(())", "()()()"
+*/
+
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<String>();
