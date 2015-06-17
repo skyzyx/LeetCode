ec561879fc2a1cc58e24979934505372cb5be0e9
Jie Zhang <jiez@ucsc.edu>
2015-06-17 14:29:48 -0700

    Longest substring without repeating characters.

--- 3. Longest Substring Without Repeating Characters/Solution.cpp
+++ 3. Longest Substring Without Repeating Characters/Solution.cpp
@@ -1,3 +1,9 @@
+// LeetCode #3: Longest Substring Without Repeating Characters
+
+/*
+Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+*/
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
