f6d3d1683a53b8e21ebcce24c0b94dd8c865cfe0
Jie Zhang <jiez@ucsc.edu>
2015-06-18 18:05:00 -0700

    Longest valid parentheses.

--- 32. Longest Valid Parentheses/Solution.py
+++ 32. Longest Valid Parentheses/Solution.py
@@ -1,3 +1,13 @@
+# LeetCode #32: Longest Valid Parentheses
+
+'''
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+'''
+
 class Solution:
     # @param {string} s
     # @return {integer}
