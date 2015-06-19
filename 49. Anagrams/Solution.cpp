c6dbe48b546b1fd7df9291060851ec17339f23d3
Jie Zhang <jiez@ucsc.edu>
2015-06-18 18:07:03 -0700

    Given an array of strings, return all groups of strings that are anagrams.

--- 49. Anagrams/Solution.cpp
+++ 49. Anagrams/Solution.cpp
@@ -1,3 +1,11 @@
+// LeetCode #49: Anagrams
+
+/*
+Given an array of strings, return all groups of strings that are anagrams.
+
+Note: All inputs will be in lower-case.
+*/
+
 class Solution {
 public:
     vector<string> anagrams(vector<string>& strs) {
