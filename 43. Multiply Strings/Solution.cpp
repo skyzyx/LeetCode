140211f5e118dea3ba7e3acdf6906234d3401f97
Jie Zhang <jiez@ucsc.edu>
2015-06-19 22:40:53 -0700

    Given two numbers representated as strings, return multiplication of the numbers as a string.

--- 43. Multiply Strings/Solution.cpp
+++ 43. Multiply Strings/Solution.cpp
@@ -1,3 +1,11 @@
+// LeetCode #43: Multiply Strings
+
+/*
+Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+Note: The numbers can be arbitrarily large and are non-negative.
+*/
+
 class Solution {
 public:
     string multiply(string num1, string num2) {
