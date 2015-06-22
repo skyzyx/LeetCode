341babf36ac4aa35a9b0ad3a1c7789f8f645446a
Jie Zhang <jiez@ucsc.edu>
2015-06-21 21:01:44 -0700

    Largest number.

--- 179. Largest Number/Solution.java
+++ 179. Largest Number/Solution.java
@@ -1,3 +1,13 @@
+// LeetCode #179: Largest Number
+
+/*
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+
+Note: The result may be very large, so you need to return a string instead of an integer.
+*/
+
 public class Solution {
     public String largestNumber(int[] num) {
         if (num == null || num.length == 0)
