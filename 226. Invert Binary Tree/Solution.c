14a857b8313564014ce3287c0ce1f31a6cb3fd6a
Jie Zhang <jiez@ucsc.edu>
2015-06-23 12:29:22 -0700

    Invert binary tree.

---
+++ 226. Invert Binary Tree/Solution.c
@@ -0,0 +1,41 @@
+// LeetCode #226: Invert Binary Tree
+
+/*
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if (root == NULL)
+        return root;
+        
+    struct TreeNode* left = root->left;
+    struct TreeNode* right = root->right;
+    
+    root->left = invertTree(right);
+    root->right = invertTree(left);
+    return root;
+}
