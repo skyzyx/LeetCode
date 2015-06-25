e6e7534076d6367fd64bee8c180779e84873c0a5
Jie Zhang <jiez@ucsc.edu>
2015-06-24 23:50:06 -0700

    Given a 2D binary matrix filled with 0's and 1's, find the largest square containing all 1's and return its area.

--- 221. Maximal Square/Solution.java
+++ 221. Maximal Square/Solution.java
@@ -25,7 +25,7 @@ public class Solution {
                 if (i > 0 && j > 0 && matrix[i][j] == '1') {
                     dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
                 } else {
-                    dp[i][j] = matrix[i][j] - '0';
+                    dp[i][j] = matrix[i][j] - '0'; // Did not work: dp[i][j] = (int)matrix[i][j], char to int
                 }
                 
                 maxLen = Math.max(maxLen, dp[i][j]);
