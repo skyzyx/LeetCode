c72cda9f7a7d75e161d376ec540cf95f7ebcb921
Jie Zhang <jiez@ucsc.edu>
2015-06-24 23:50:31 -0700

    Given a 2D binary matrix filled with 0's and 1's, find the largest square containing all 1's and return its area.

--- 221. Maximal Square/Solution.cpp
+++ 221. Maximal Square/Solution.cpp
@@ -27,7 +27,7 @@ public:
                 if (i > 0 && j > 0 && matrix[i][j] == '1')
                     dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
                 else
-                    dp[i][j] = matrix[i][j] - '0';
+                    dp[i][j] = matrix[i][j] - '0'; // Did not work: dp[i][j] = (int)matrix[i][j];
                     
                 maxLen = max(maxLen, dp[i][j]);
             }
