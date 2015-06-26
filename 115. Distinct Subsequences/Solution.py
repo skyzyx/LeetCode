1b8ef667c27f5a1106162fd1dde53801c25584a8
Jie Zhang <jiez@ucsc.edu>
2015-06-26 13:01:31 -0700

    Count the numbers of distinct substrings of T in S.

--- 115. Distinct Subsequences/Solution.py
+++ 115. Distinct Subsequences/Solution.py
@@ -10,7 +10,28 @@ S = "rabbbit", T = "rabbit"
 
 Return 3.
 '''
+class Solution:
+    def numDistinct(self, s, t):
+        m = len(s)
+        n = len(t)
+        dp = [[0 for i in xrange(n+1)] for j in xrange(m+1)]
+        
+        for i in xrange(n+1):
+            dp[0][i] = 0
+        for i in xrange(m+1):
+            dp[i][0] = 1
+            
+        for i in xrange(1, m+1):
+            for j in xrange(1, n+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+                else:
+                    dp[i][j] = dp[i-1][j]
+                    
+        return dp[m][n]
 
+
+'''
 class Solution: # Time Limit Exceeded
     # @param {string} s
     # @param {string} t
@@ -31,3 +52,4 @@ class Solution: # Time Limit Exceeded
                 res += self.numDistinct(s[i+1:], t[1:])
                 
         return res;
+'''
