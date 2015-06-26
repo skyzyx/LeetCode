b4b7e2f32ebecb8de3e241f9ee5f90ed1bb9fb22
Jie Zhang <jiez@ucsc.edu>
2015-06-26 13:02:38 -0700

    Count the numbers of distinct substrings of T in S.

--- 115. Distinct Subsequences/Solution.java
+++ 115. Distinct Subsequences/Solution.java
@@ -11,6 +11,31 @@ S = "rabbbit", T = "rabbit"
 Return 3.
 */
 
+public class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m+1][n+1];
+        
+        for (int i = 0; i < n+1; i++)
+            dp[0][i] = 0;
+        for (int i = 0; i < m+1; i++)
+            dp[i][0] = 1;
+            
+        for (int i = 1; i < m+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if (s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[m][n];
+    }
+}
+
+
+/*
 public class Solution { // Time Limit Exceeded
     public int numDistinct(String s, String t) {
         if (s == null || t == null)
@@ -30,3 +55,4 @@ public class Solution { // Time Limit Exceeded
         return res;
     }
 }
+*/
