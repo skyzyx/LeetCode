7f199ce3faea542a940989622c2bbb82a6ad2bbd
Jie Zhang <jiez@ucsc.edu>
2015-06-26 13:02:06 -0700

    Count the numbers of distinct substrings of T in S.

--- 115. Distinct Subsequences/Solution.cpp
+++ 115. Distinct Subsequences/Solution.cpp
@@ -11,6 +11,31 @@ S = "rabbbit", T = "rabbit"
 Return 3.
 */
 
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        
+        int dp[m+1][n+1];
+        for (int i = 0; i < n+1; i++)
+            dp[0][i] = 0;
+        for (int i = 0; i < m+1; i++)
+            dp[i][0] = 1;
+            
+        for (int i = 1; i < m+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if (s[i-1] == t[j-1])
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+/*
 class Solution { // Time Limit Exceeded
 public:
     int numDistinct(string s, string t) {
@@ -28,3 +53,4 @@ public:
         return res;
     }
 };
+*/
