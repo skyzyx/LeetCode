41340a94e07a1fd81d9bf53fe0f7e24b43797c11
Jie Zhang <jiez@ucsc.edu>
2015-06-27 23:20:51 -0700

    Dungeon game.

---
+++ 174. Dungeon Game/Solution.java
@@ -0,0 +1,23 @@
+// LeetCode #174: Dungeon Game
+
+public class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        
+        int[][] dp = new int[m][n];
+        dp[m-1][n-1] = Math.max(1, 1-dungeon[m-1][n-1]);
+        
+        for (int i = m-2; i >= 0; i--)
+            dp[i][n-1] = Math.max(1, dp[i+1][n-1] - dungeon[i][n-1]);
+            
+        for (int j = n-2; j >= 0; j--)
+            dp[m-1][j] = Math.max(1, dp[m-1][j+1] - dungeon[m-1][j]);
+            
+        for (int i = m-2; i >= 0; i--)
+            for (int j = n-2; j >= 0; j--)
+                dp[i][j] = Math.max(1, Math.min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]);
+                
+        return dp[0][0];
+    }
+}
