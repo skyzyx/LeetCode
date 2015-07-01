8ae3ba0f3af1c5b08245b8529cb4bf01132e8ebe
Jie Zhang <jiez@ucsc.edu>
2015-06-30 20:41:49 -0700

    String to integer (atoi).

---
+++ 8. String to Integer(atoi)/Solution.py
@@ -0,0 +1,58 @@
+# LeetCode #8: String to Integer (atoi)
+
+'''
+Implement atoi to convert a string to an integer.
+
+Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+
+Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+
+Update (2015-02-10):
+The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
+
+spoilers alert... click to show requirements for atoi.
+
+Requirements for atoi:
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+'''
+
+class Solution:
+    # @param {string} str
+    # @return {integer}
+    def myAtoi(self, str):
+        INT_MAX = 2147483647
+        INT_MIN = -2147483648
+        
+        maxDiv10 = INT_MAX / 10
+        
+        i = 0
+        n = len(str)
+        
+        while i < n and str[i] == ' ':
+            i += 1
+            
+        sign = 1
+        if i < n and str[i] == '+':
+            i += 1
+        elif i < n and str[i] == '-':
+            i += 1
+            sign = -1
+            
+        res = 0
+        while i < n and str[i].isdigit():
+            digit = int(str[i])
+            if res > maxDiv10 or (res == maxDiv10 and digit >= 8):
+                return INT_MAX if sign == 1 else INT_MIN
+                
+            res = res * 10 + digit
+            i += 1
+            
+        return res * sign
+            
+        
