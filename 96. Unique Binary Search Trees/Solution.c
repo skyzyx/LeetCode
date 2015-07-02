bb481d92852f3d16d754010ada00a573c19f0b18
Jie Zhang <jiez@ucsc.edu>
2015-07-02 13:35:07 -0700

    Unique binary search trees.

--- 96. Unique Binary Search Trees/Solution.c
+++ 96. Unique Binary Search Trees/Solution.c
@@ -18,11 +18,18 @@ int numTrees(int n) {
         return 0;
         
     int* dp; // int dp[n+1] = {0}; Line 5: variable-sized object may not be initialized
+    
+    int i = 0;
+    while (i < n+1) {
+        dp[i] = 0;
+        i++;
+    }
+    
     dp[0] = 1;
-    dp[1] = 1;
-    for (int i = 2; i <= n; i++) {
-        for (int j = 0; j < i; j++) {
-            dp[i] += dp[j] * dp[i-j-1];
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i] += dp[j-1] * dp[i-j];
         }
     }
     return dp[n];  
