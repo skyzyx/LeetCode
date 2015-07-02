ec554ee33ffd171ae193e75e376597dddd07992f
Jie Zhang <jiez@ucsc.edu>
2015-07-02 13:35:03 -0700

    Unique binary search trees.

--- 96. Unique Binary Search Trees/Solution.py
+++ 96. Unique Binary Search Trees/Solution.py
@@ -19,11 +19,11 @@ class Solution:
         if n <= 0:
             return 0
         
-        dp = [0] * (n+1)
+        dp = [0 for i in xrange(n+1)]
         dp[0] = 1;
-        dp[1] = 1;
-        for i in range(2, n+1):
-            for j in range(0, i):
-                dp[i] += dp[j] * dp[i-j-1]
+
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                dp[i] += dp[j-1] * dp[i-j]
         return dp[n]
         
