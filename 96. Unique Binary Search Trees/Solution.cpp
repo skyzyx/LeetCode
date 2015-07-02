7aa0720aab3eef5cb14d05e58dbb034fb97dcc2f
Jie Zhang <jiez@ucsc.edu>
2015-07-02 13:34:51 -0700

    Unique binary search trees.

--- 96. Unique Binary Search Trees/Solution.cpp
+++ 96. Unique Binary Search Trees/Solution.cpp
@@ -21,10 +21,10 @@ public:
         
         int dp[n+1] = {0};
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                dp[i] += dp[j] * dp[i-j-1];
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j-1] * dp[i-j];
             }
         }
         return dp[n];
