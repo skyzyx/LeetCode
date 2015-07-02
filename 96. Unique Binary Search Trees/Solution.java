363549656259bcf4e41ea87ecb79aa39e3f45589
Jie Zhang <jiez@ucsc.edu>
2015-07-02 13:34:57 -0700

    Unique binary search trees.

--- 96. Unique Binary Search Trees/Solution.java
+++ 96. Unique Binary Search Trees/Solution.java
@@ -13,17 +13,17 @@ Given n = 3, there are a total of 5 unique BST's.
    2     1         2                 3
 */
 
-public class Solution {
+public class Solution { // Dynamic Programming
     public int numTrees(int n) {
         if (n <= 0)
             return 0;
             
         int[] dp = new int[n+1];
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <=n; i++) {
-            for (int j = 0; j < i; j++) {
-                dp[i] += dp[j] * dp[i-j-1];
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j-1] * dp[i-j];
             }
         }
         return dp[n];
@@ -31,7 +31,7 @@ public class Solution {
 }
 
 /*
-public class Solution {
+public class Solution { // Recursion
     public int numTrees(int n) {
         if (n < 0)
             return 0;
