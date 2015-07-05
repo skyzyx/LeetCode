938cdd0d92d28293187ea8018cda97db103e22ed
Jie Zhang <jiez@ucsc.edu>
2015-07-05 14:19:22 -0700

    Given an integer, write a function to determine if it is a power of two.

---
+++ 231. Power of Two/Solution.c
@@ -0,0 +1,7 @@
+// LeetCode #231: Power of Two
+
+// Given an integer, write a function to determine if it is a power of two.
+
+bool isPowerOfTwo(int n) {
+    return n > 0 && ((n & (n-1)) == 0);
+}
