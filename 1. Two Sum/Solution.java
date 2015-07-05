ee87cb65b9e0addac686cb6e4e2a4cfe494cf7fe
Jie Zhang <jiez@ucsc.edu>
2015-07-04 21:06:06 -0700

    Two sum.

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -10,6 +10,20 @@ You may assume that each input would have exactly one solution.
 Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))
+                return new int[] {map.get(target - nums[i]) + 1, i+1};
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+/*
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] res = new int[2];
@@ -26,3 +40,5 @@ public class Solution {
         return res;
     }
 }
+*/
+
