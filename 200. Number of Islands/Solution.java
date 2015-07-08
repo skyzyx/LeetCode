4e678e3219b6bd1abfd8b30f5a9397c34aa9506e
Jie Zhang <jiez@ucsc.edu>
2015-07-07 20:45:14 -0700

    Number of islands.

--- 200. Number of Islands/Solution.java
+++ 200. Number of Islands/Solution.java
@@ -20,15 +20,14 @@ Example 2:
 Answer: 3
 */
 
-class Solution {
-public:
-    int numIslands(vector<vector<char>> &grid) {
-        if (grid.size() == 0 || grid[0].size() == 0)
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0)
             return 0;
             
         int res = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
                     res++;
                     dfs(grid, i, j);
@@ -38,18 +37,17 @@ public:
         return res;
     }
     
-private:
-    void dfs(vector<vector<char>> &grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
             return;
         if (grid[i][j] != '1')
             return;
             
-        grid[i][j] = '-';
+        grid[i][j] = '-'; // - means visited.
         
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
         dfs(grid, i-1, j);
         dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
     }
-};
+}
