6561cb0468d817404c94a077d6b096ad7101550e
Jie Zhang <jiez@ucsc.edu>
2015-07-28 10:38:04 -0700

    Two sum, data structure design.

--- 170. Two Sum III - Data structure design/Solution.java
+++ 170. Two Sum III - Data structure design/Solution.java
@@ -1,3 +1,17 @@
+// LeetCode #170: Two Sum III
+
+/*
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+For example,
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+*/
+
 public class TwoSum {
     private Map<Integer, Integer> map = new HashMap<Integer,Integer>();
 
@@ -11,11 +25,8 @@ public class TwoSum {
 	public boolean find(int value) {
 	    for (Integer key: map.keySet()) {
 	        int target = value - key;
-	        if (map.containsKey(target)) {
-	            if (target == key && map.get(key) < 2)
-	                continue;
+	        if (map.containsKey(target) && (target != key || map.get(target) > 1))
 	           return true;
-	        }
 	    }
 	    return false;
 	}
