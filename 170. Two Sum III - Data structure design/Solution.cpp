559f7c154498fe2141a3cc3af6284d46ebf9d9f4
Jie Zhang <jiez@ucsc.edu>
2015-07-27 22:37:05 -0700

    Two sum, data structure design.

---
+++ 170. Two Sum III - Data structure design/Solution.cpp
@@ -0,0 +1,34 @@
+// LeetCode #170: Two Sum III
+
+/*
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+For example,
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+*/
+
+class TwoSum {
+private:
+    unordered_map<int, int> map;
+
+public:
+	void add(int number) {
+        map[number]++;	    
+	}
+
+	bool find(int value) {
+	    for (auto it = map.begin(); it != map.end(); it++) {
+	        int target = value - it->first;
+	        if (map.find(target) != map.end() && target != it->first)
+	            return true;
+	        else if (map.find(target) != map.end() && target == it->first && map[target] >= 2)
+	            return true;
+	    }
+	    return false;
+	}
+};
