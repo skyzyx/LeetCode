3be65cb4615d4ff4404041afdec2b5f11de8dbff
Jie Zhang <jiez@ucsc.edu>
2015-07-30 21:12:50 -0700

    Determine if a linked list is a palindrome.

--- 234. Palindrome Linked List/Solution.java
+++ 234. Palindrome Linked List/Solution.java
@@ -17,6 +17,42 @@ Could you do it in O(n) time and O(1) space?
  */
 public class Solution {
     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+            
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        ListNode head2 = slow.next;
+
+        ListNode prev = null;
+        while (head2 != null) {
+            ListNode next = head2.next;
+            head2.next = prev;
+            prev = head2;
+            head2 = next;
+        }
+        
+        ListNode p1 = head;
+        ListNode p2 = prev;
+        
+        while (p2 != null) {
+            if (p2.val != p1.val)
+                return false;
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+        return true;
+    }
+}
+
+/*
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
         
         Stack<ListNode> s = new Stack<ListNode>();
@@ -34,3 +70,4 @@ public class Solution {
         return true;
     }
 }
+*/
