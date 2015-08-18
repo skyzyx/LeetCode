49e273b761d4e93aa36e015c5a10d18bf421251c
Jie Zhang <jiez@ucsc.edu>
2015-08-17 22:24:12 -0700

    Group anagrams.

--- 49. Anagrams/Solution.py
+++ 49. Anagrams/Solution.py
@@ -1,11 +1,43 @@
 # LeetCode #49: Anagrams
 
 '''
-Given an array of strings, return all groups of strings that are anagrams.
+Given an array of strings, group anagrams together.
 
-Note: All inputs will be in lower-case.
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+Note:
+For the return value, each inner list's elements must follow the lexicographic order.
+All inputs will be in lower-case.
+
+Update (2015-08-09):
+The signature of the function had been updated to return list<list<string>> instead of 
+list<string>, as suggested here. If you still see your function signature return a 
+list<string>, please click the reload button  to reset your code definition.
 '''
 
+class Solution:
+    # @param {string[]} strs
+    # @return {string[][]}
+    def groupAnagrams(self, strs):
+        res = []
+        dic = {}
+        for word in strs:
+            sword = ''.join(sorted(word))
+            dic[sword] = [word] if sword not in dic else dic[sword] + [word]
+            
+        for key in dic:
+            res.append(sorted(dic[key]))
+            
+        return res
+
+'''
 class Solution:
     # @param {string[]} strs
     # @return {string[]}
@@ -21,5 +53,4 @@ class Solution:
                 res = res + dic[elem]
                 
         return res
-            
-        
+'''       
